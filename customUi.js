3d19706a91ea04220a2d97bbf8d95d016aab5dce
Jill Arnold <arnoldj1@michigan.gov>
2013-01-30 15:15:23 -0500

    Trying again.

--- customUi.js
+++ customUi.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
 	//$('#header_wrapper').append('<div id="header" />');
 	/*$('#main_wrapper').append('<div id="header_wrapper" />');*/
 	$('#header_wrapper').append('<div id="header" />');
-	$('#main_wrapper').html(navbar);
+	$('#main_wrapper').append(navbar);
 	
 	//move the search box & style
 	$("#search").appendTo("#header");
